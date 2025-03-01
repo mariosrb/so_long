6f94f40b30beaaafbe0841c16b50ec6ea0ae2ba7
mariosrb <mario-2000@hotmail.fr>
2025-03-01 19:17:48 +0100

    graphic init !

--- src/graphics/graphics_utils.c
+++ src/graphics/graphics_utils.c
@@ -6,7 +6,65 @@
 /*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 23:10:24 by mario             #+#    #+#             */
-/*   Updated: 2025/02/24 23:10:25 by mario            ###   ########.fr       */
+/*   Updated: 2025/03/01 17:46:10 by mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../../include/so_long.h"
+
+void    init_graphics_struct(t_graphics *graphics)
+{
+    graphics->mlx = NULL;
+    graphics->window = NULL;
+    graphics->textures.player = NULL;
+    graphics->textures.exit = NULL;
+    graphics->textures.collect = NULL;
+    graphics->textures.floor = NULL;
+    graphics->textures.wall = NULL;
+    graphics->width = 0;
+    graphics->height = 0;
+}
+
+void    graphics_destroy(t_graphics *graphics)
+{
+    if (!graphics)
+        return ;
+    if (graphics->mlx)
+    {
+        if (graphics->textures.player)
+        {
+            mlx_destroy_image(graphics->mlx, graphics->textures.player);
+            graphics->textures.player = NULL;
+        }
+        if (graphics->textures.exit)
+        {
+            mlx_destroy_image(graphics->mlx, graphics->textures.exit);
+            graphics->textures.exit = NULL;
+        }
+        if (graphics->textures.collect)
+        {
+            mlx_destroy_image(graphics->mlx, graphics->textures.collect);
+            graphics->textures.collect = NULL;
+        }
+        if (graphics->textures.floor)
+        {
+            mlx_destroy_image(graphics->mlx, graphics->textures.floor);
+            graphics->textures.floor = NULL;
+        }
+        if (graphics->textures.wall)
+        {
+            mlx_destroy_image(graphics->mlx, graphics->textures.wall);
+            graphics->textures.wall = NULL;
+        }
+        if (graphics->window)
+        {
+            mlx_destroy_window(graphics->mlx, graphics->window);
+            graphics->window = NULL;
+        }
+        mlx_destroy_display(graphics->mlx);
+        free(graphics->mlx);
+        graphics->mlx = NULL;
+    }
+    free(graphics);
+}
 
--- src/main.c
+++ src/main.c
@@ -6,26 +6,26 @@
 /*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 23:03:28 by mario             #+#    #+#             */
-/*   Updated: 2025/02/28 00:02:12 by mario            ###   ########.fr       */
+/*   Updated: 2025/02/28 15:33:39 by mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-int main(int ac, char **av)
-{
-    t_map map;
-    int error_code;
+// int main(int ac, char **av)
+// {
+//     t_map map;
+//     int error_code;
     
-    (void)ac;
-    if (!map_load(&map, av[1]))
-        return (1);
-    error_code = validate_map(&map);
-    if (error_code != MAP_OK)
-    {
-        free_char_tab(map.map);
-        return (1);
-    }
-    free_char_tab(map.map);
-    return (0);
-}
+//     (void)ac;
+//     if (!map_load(&map, av[1]))
+//         return (1);
+//     error_code = validate_map(&map);
+//     if (error_code != MAP_OK)
+//     {
+//         free_char_tab(map.map);
+//         return (1);
+//     }
+//     free_char_tab(map.map);
+//     return (0);
+// }
